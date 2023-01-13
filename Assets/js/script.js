2ad5129b1017d131f69da3500047d67a22709b55
InfoTech SOMIK <skitech226@gmail.com>
2023-01-13 11:45:58 +0100

    responsivite du site

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+function menuToggle(){
+    let hamburger = document.querySelector('#hamburger' );
+    let header = document.querySelector('#header');
+    let icon = hamburger.firstChild;
+
+    hamburger.addEventListener('click', menuHandle)
+    function menuHandle(){
+        if (!header.classList.contains('show')){
+            header.classList.add('show');
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        }else{
+            header.classList.remove('show');
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    }
+    console.log(hamburger,header,icon);
+}
+
+menuToggle();
